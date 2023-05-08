e92dc892735db6ea6fe7c072481766f03c0656db
raju dey <rajudey432@gmail.com>
2023-05-08 19:30:33 +0530

    Welcome to user registration problem

---
+++ UserRegistrationProblem/Main.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.UserRegistrationProblem;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to user registration problem");
+    }
+}
