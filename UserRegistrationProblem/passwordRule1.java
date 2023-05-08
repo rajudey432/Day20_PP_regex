8bbba28cdaf0a5316b4ca3719403968a64c289b7
raju dey <rajudey432@gmail.com>
2023-05-08 19:52:02 +0530

    Added: Password rule1

---
+++ UserRegistrationProblem/passwordRule1.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.UserRegistrationProblem;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class passwordRule1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String password;
+
+        // Define the regular expression pattern
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]{8,}$");
+
+        // Prompt the user for input until a valid password is entered
+        do {
+            System.out.print("Enter your password: ");
+            password = scanner.nextLine();
+        } while (!pattern.matcher(password).matches());
+
+        System.out.println("Your password is " + password);
+    }
+}
+
